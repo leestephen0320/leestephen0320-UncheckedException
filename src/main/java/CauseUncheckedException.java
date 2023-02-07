472b85b64ca22ff34ea1e5cbd31ee5473b20cc5e
leestephen0320 <leestephen0320@gmail.com>
2023-02-07 23:47:33 +0000

    modified:   src/main/java/CauseUncheckedException.java

--- src/main/java/CauseUncheckedException.java
+++ src/main/java/CauseUncheckedException.java
@@ -14,6 +14,7 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int i = 2;
+        i/0;
     }
 }
